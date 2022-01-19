87bfdd80fe62607bff0d4a885d1728da02092b12
Tugrul Agrikli <tagrikli@gmail.com>
2022-01-19 21:39:23 +0300

    dbus

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('selalar')
+from pydbus import SystemBus
+
+bus = SystemBus()
+
+adapter = bus.get('org.bluez', '/org/bluez/hci0')
+mngr = bus.get('org.bluez', '/')
+
+
+def list_connected_devices():
+    mngd_objs = mngr.GetManagedObjects()
+    for path in mngd_objs:
+        con_state = mngd_objs[path].get(
+            'org.bluez.Device1', {}).get('Connected', False)
+        if con_state:
+            addr = mngd_objs[path].get('org.bluez.Device1', {}).get('Address')
+            name = mngd_objs[path].get('org.bluez.Device1', {}).get('Name')
+            print(f'Device {name} [{addr}] is connected')
+
+
+if __name__ == '__main__':
+    list_connected_devices()
